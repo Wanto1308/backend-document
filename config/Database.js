6dd7c7e9a5740b6998eb8026f9a781353d2e34bb
unknown <the.1.wanto@gmail.com>
2022-07-23 19:47:28 +0700

    adjust server database mysql

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('document', 'root', '', {
-  host: 'localhost',
+const db = new Sequelize('sql6508243', 'sql6508243', 'tyriC4bWcY', {
+  host: 'sql6.freemysqlhosting.net',
   dialect: 'mysql'
 });
 
