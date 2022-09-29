2e196276523381270aac3a8ba4119837027368b7
unknown <the.1.wanto@gmail.com>
2022-09-29 13:20:44 +0700

    Change database DomaiNesia

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,13 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('2aZMBvPUBE', '2aZMBvPUBE', '3eydo9Qkru', {
-  host: 'remotemysql.com',
-  dialect: 'mysql'
-});
-
-// const db = new Sequelize('arsipfil_upload_db', 'arsipfil_arsip-file', 'Imanuel123456', {
-//   host: 'catelyn.id.rapidplex.com',
+// const db = new Sequelize('2aZMBvPUBE', '2aZMBvPUBE', '3eydo9Qkru', {
+//   host: 'remotemysql.com',
 //   dialect: 'mysql'
 // });
+
+const db = new Sequelize('arsipfil_upload_db', 'arsipfil_arsip-file', 'Imanuel123456', {
+  host: 'catelyn.id.rapidplex.com',
+  dialect: 'mysql'
+});
 
 export default db;
