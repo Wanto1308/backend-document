54ff8a5ccc2d54efc5c7307d2c1e1796e41fa26d
unknown <the.1.wanto@gmail.com>
2023-04-29 08:03:52 +0700

    adjust new database

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-// const db = new Sequelize('2aZMBvPUBE', '2aZMBvPUBE', '3eydo9Qkru', {
-//   host: 'remotemysql.com',
-//   dialect: 'mysql'
+// const db = new Sequelize('document', 'root', '', {
+//   host: 'localhost',
+//   dialect: "mysql"
 // });
 
-const db = new Sequelize('arsipfil_upload_db', 'arsipfil_arsip-file', 'Imanuel123456', {
-  host: 'catelyn.id.rapidplex.com',
+const db = new Sequelize('myar9880_arsip', 'myar9880_iman', 'imanueltoing', {
+  host: 'srv79.niagahoster.com',
   dialect: 'mysql'
 });
 
